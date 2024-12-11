2c38acbeb8d2c7ce936454b6b6539faa11720c1a
aneethsolomonDev <aneethsolomon2001@gmail.com>
2024-12-11 09:00:18 +0530

    Add files via upload

---
+++ sqlmaster.sql
@@ -0,0 +1,8 @@
+select * from v$database; --Displays information about the database, such as name and creation date.
+select * from v$instance; --Provides details about the Oracle instance, including status and version.
+select * from v$session; --Shows information about currently connected database sessions.
+select * from v$sql; --Displays SQL statements that are cached in the shared pool.
+select * from v$tablespace; --Provides information about tablespaces in the database.
+select * from v$process; --Displays details about processes running in the database.
+select * from gv$parameter; --Lists current initialization parameters and their values.
+
